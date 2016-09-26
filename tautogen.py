ca7780e60710e61c992e1b8326253897b9acabd5
William Knowles <will@hollyandwill.com>
2016-09-26 17:45:25 -0400

    add heroku config and tweepy api

--- tautogen.py
+++ tautogen.py
@@ -5,15 +5,16 @@
 
 import tweepy, random
 
-# Not needed for heroku - twitter auth is done through config vars added to heroku
-#
-# # import twitter creds from secrets.py
-# from secrets import *
-#
-# # twitter auth
-# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-# api = tweepy.API(auth)
+# import auth from heroku
+CONSUMER_KEY = os.environ['CONSUMER_KEY']
+CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = os.environ['ACCESS_TOKEN_SECRET']
+
+# twitter auth
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
 # bot setup
 date = random.randrange(1735, 2016)
