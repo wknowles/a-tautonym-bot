8797c3813c9286f6a14f99172df52453e729e713
William Knowles <will@hollyandwill.com>
2016-09-26 18:30:34 -0400

    clean up and add readme

--- tautogen.py
+++ tautogen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# taxonomy taken from list of Classical Latin proper names: https://github.com/cltk/latin_proper_names_cltk
-
 import tweepy, os, random
+from datetime import date
+
 
 # import auth from heroku
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
@@ -17,16 +17,14 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 # bot setup
-date = random.randrange(1735, 2016)
+date = random.randrange(1735, date.today().year)
 genus = random.choice(open('corpus/proper_names.txt').readlines()).rstrip('\n').lower()
 binomial = genus.title() + " " + genus
 authority = random.choice(open('corpus/authority.txt').readlines()).rstrip('\n')
-#TODO: perhaps have random parenthesis around authority and date - the parentheses indicate that the species is now considered to belong in a different genus
 tweet = "%s (%s, %i)" % (binomial, authority, date)
 
 # check length and then tweet
 if len(tweet)<=140 and len(tweet)>0:
-    print "Tweeting ====> ", tweet
     api.update_status(tweet)
 else:
     print "Skipped tweet - too long!"
